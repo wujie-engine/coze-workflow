24b3c45f91744ab41c2382d6ca6ba9689898a809
z <1837800317@qq.com>
2025-12-08 21:45:18 +0800

    feat: init

---
+++ scripts/init.sh
@@ -0,0 +1,6 @@
+echo "🚀 初始化远程仓库"
+
+git remote add github git@github.com:wujie-engine/coze-workflow.git
+git remote add gitee git@gitee.com:wujie-engine/coze-workflow.git
+
+echo "🚀 初始化远程仓库完成"
---
+++ scripts/push.sh
@@ -0,0 +1,10 @@
+echo "🚀 推送代码到 GitLab"
+git push
+
+echo "🚀 推送代码到 GitHub"
+git push github
+
+echo "🚀 推送代码到 Gitee"
+git push gitee
+
+echo "🚀 推送代码完成"
